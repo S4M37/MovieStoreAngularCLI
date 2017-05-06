d6e1994c261d7c2fc0a695bfb9323462283d02d6
S4M37 <sametoussema@gmail.com>
2017-05-06 03:58:24 +0200

    fixing endpoints

--- src/app/shared/Endpoints.ts
+++ src/app/shared/Endpoints.ts
@@ -1,6 +1,6 @@
 export class Endpoints {
 
-  public static readonly API_ENDPOINT = 'http://localhost:3000/';
+  public static readonly API_ENDPOINT = 'http://ec2-34-248-57-210.eu-west-1.compute.amazonaws.com:3000/';
   public static readonly ACTORS_ENDPOINT = Endpoints.API_ENDPOINT + 'actors';
   public static readonly MOVIES_ENDPOINT = Endpoints.API_ENDPOINT + 'movies';
   static MOVIE_SEARCH = Endpoints.API_ENDPOINT + 'elastic/movies/search';
